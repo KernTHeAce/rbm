d608c9a8a10981d7f31aa5c5575d56fedeb717a9
KernTHeAce <vanatuz2@gmail.com>
2023-04-21 14:10:40 +0300

    fix: mlflow_registry

--- src/nodes/save_load/mlflow_registry.py
+++ src/nodes/save_load/mlflow_registry.py
@@ -14,13 +14,14 @@ def mlflow_registry(experiment_name, epoch: int, metrics=None, new_experiment=Tr
         experiment_id = mlflow.create_experiment(experiment_name)
 
     if metrics:
-        metrics_data = "   ".join([f"{key}: {item[mc.VALUE]}" for key, item in metrics.items()])
+        metrics_data = "   ".join([f"{key}: {round(item[mc.VALUE], 3)}" for key, item in metrics.items()])
     else:
         metrics_data = ""
     mlflow.set_experiment(experiment_name)
     with mlflow.start_run():
         mlflow.set_tag("mlflow.runName", f"Epoch: {epoch} {metrics_data}")
-        mlflow.log_metric("param", value=230)
+        for key, item in metrics.items():
+            mlflow.log_metric(key, value=item[mc.VALUE])
     return cc.NONE
 
 
