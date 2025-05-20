e7f95c12a16768cd93672ed6996e2b46fb63c441
ivan.tuz <ivan.tuz@activecloud.com>
2025-05-20 16:55:58 +0300

    feat: crop dataset experiments

--- src/experiments/crop_ae/__main__.py
+++ src/experiments/crop_ae/__main__.py
@@ -12,22 +12,21 @@ from src.experiments import INITIALIZERS
 
 
 class CustomDataset(Dataset):
-    def __init__(self, csv_file, categorical_col, label_col):
+    def __init__(self, csv_file, label_col):
         self.data = pd.read_csv(csv_file)
 
         yes_no_cols = self.data.select_dtypes(include=['object']).columns
         for col in yes_no_cols:
             if set(self.data[col].unique()) <= {"Yes", "No"}:
                 self.data[col] = self.data[col].map({"Yes": 1, "No": 0})
-        features_data = self.data.drop(columns=[label_col, categorical_col])
+        features_data = self.data.drop(columns=[label_col])
         numeric_cols = features_data.select_dtypes(include=['float']).columns
         features_numeric = features_data[numeric_cols].values.astype('float32')
         bool_cols = features_data.select_dtypes(include=['int']).columns
         features_bool = features_data[bool_cols].values.astype('float32')
         self.encoder = OneHotEncoder(sparse_output=False, handle_unknown='ignore')
-        categorical_data = self.encoder.fit_transform(self.data[[categorical_col]])
         self.features = torch.tensor(
-            np.concatenate([features_numeric, features_bool, categorical_data], axis=1), dtype=torch.float64
+            np.concatenate([features_numeric, features_bool], axis=1), dtype=torch.float64
         )
 
     def __len__(self):
@@ -38,10 +37,10 @@ class CustomDataset(Dataset):
 
 
 dataset = CustomDataset(
-    csv_file=f"{DATA_DIR}/heart_disease/2020/heart_2020_cleaned.csv",
-    categorical_col="AgeCategory",
-    label_col="HeartDisease"
+    csv_file=f"{DATA_DIR}/crop/Crop_recommendation.csv",
+    label_col="label"
 )
+
 
 train_size = int(0.8 * len(dataset))
 test_size = len(dataset) - train_size
@@ -52,14 +51,14 @@ train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
 
-lengths = {"s": [5], "m": [10, 7, 10], "l": [15, 10, 7, 10, 15]}
+lengths = {"s": [2], "m": [5, 3, 5], "l": [7, 5, 3, 5, 7]}
 model_combinations = generate_combinations(
     {
         "l": ["s", "m", "l"],
         "w_k": [1, 7, 15],
     }
 )
-MODEL_INPUT_SIZE = 25
+MODEL_INPUT_SIZE = 7
 metrics_calculator = MetricCalculator([regression.mae()])
 
 for model_params in model_combinations:
@@ -74,7 +73,7 @@ for model_params in model_combinations:
             response = run_experiment(
                 test_loader,
                 train_loader,
-                f"1mnist_l={model_params['l']}_wk={model_params['w_k']}",
+                f"crop_ae_l={model_params['l']}_wk={model_params['w_k']}",
                 model,
                 torch.nn.MSELoss(),
                 initializer_params,
--- src/experiments/crop_classifier/__main__.py
+++ src/experiments/crop_classifier/__main__.py
@@ -6,42 +6,43 @@ import numpy as np
 from torch.utils.data import random_split
 from src import BATCH_SIZE, DATA_DIR, DEVICE
 from custom.rbm import generate_combinations, run_experiment
-from core.metrics import MetricCalculator, regression
-from core.models import BaseModel
+from core.metrics import MetricCalculator, classification
+from core.models import Classifier
 from src.experiments import INITIALIZERS
 
 
 class CustomDataset(Dataset):
-    def __init__(self, csv_file, categorical_col, label_col):
+    def __init__(self, csv_file, label_col):
         self.data = pd.read_csv(csv_file)
+        self.labels = ['rice', 'coffee', 'maize', 'cotton', 'coffee']
 
         yes_no_cols = self.data.select_dtypes(include=['object']).columns
         for col in yes_no_cols:
             if set(self.data[col].unique()) <= {"Yes", "No"}:
                 self.data[col] = self.data[col].map({"Yes": 1, "No": 0})
-        features_data = self.data.drop(columns=[label_col, categorical_col])
+        self.labels = torch.tensor(pd.factorize(self.data[label_col].values)[0], dtype=torch.int64)
+        features_data = self.data.drop(columns=[label_col])
         numeric_cols = features_data.select_dtypes(include=['float']).columns
         features_numeric = features_data[numeric_cols].values.astype('float32')
         bool_cols = features_data.select_dtypes(include=['int']).columns
         features_bool = features_data[bool_cols].values.astype('float32')
         self.encoder = OneHotEncoder(sparse_output=False, handle_unknown='ignore')
-        categorical_data = self.encoder.fit_transform(self.data[[categorical_col]])
         self.features = torch.tensor(
-            np.concatenate([features_numeric, features_bool, categorical_data], axis=1), dtype=torch.float64
+            np.concatenate([features_numeric, features_bool], axis=1), dtype=torch.float64
         )
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        return self.features[idx]
+        return self.features[idx], self.labels[idx]
 
 
 dataset = CustomDataset(
-    csv_file=f"{DATA_DIR}/heart_disease/2020/heart_2020_cleaned.csv",
-    categorical_col="AgeCategory",
-    label_col="HeartDisease"
+    csv_file=f"{DATA_DIR}/crop/Crop_recommendation.csv",
+    label_col="label"
 )
+
 
 train_size = int(0.8 * len(dataset))
 test_size = len(dataset) - train_size
@@ -49,24 +50,23 @@ test_size = len(dataset) - train_size
 train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
 
 train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)  # shuffle=False для теста
 
 
-lengths = {"s": [5], "m": [10, 7, 10], "l": [15, 10, 7, 10, 15]}
+lengths = {"s": [7], "m": [7, 5, 3], "l": [10, 7, 5, 3]}
 model_combinations = generate_combinations(
     {
         "l": ["s", "m", "l"],
         "w_k": [1, 7, 15],
     }
 )
-MODEL_INPUT_SIZE = 25
-metrics_calculator = MetricCalculator([regression.mae()])
+MODEL_INPUT_SIZE = 7
+MODEL_OUTPUT_SIZE = 22
+metrics_calculator = MetricCalculator([classification.f1(num_classes=22)])
 
 for model_params in model_combinations:
-    model = BaseModel(
-        [MODEL_INPUT_SIZE]
-        + [item * model_params["w_k"] for item in lengths[model_params["l"]]]
-        + [MODEL_INPUT_SIZE]
+    model = Classifier(
+        [MODEL_INPUT_SIZE] + [item * model_params["w_k"] for item in lengths[model_params["l"]]] + [MODEL_OUTPUT_SIZE]
     ).to(DEVICE)
 
     for initializer_type, value in INITIALIZERS.items():
@@ -74,10 +74,13 @@ for model_params in model_combinations:
             response = run_experiment(
                 test_loader,
                 train_loader,
-                f"1mnist_l={model_params['l']}_wk={model_params['w_k']}",
+                f"crop_cl_l={model_params['l']}_wk={model_params['w_k']}",
                 model,
-                torch.nn.MSELoss(),
+                torch.nn.CrossEntropyLoss(),
                 initializer_params,
                 initializer_type,
                 metrics_calculator,
+                # lambda img: img.view(-1, 28 * 28),
+                postprocessing=lambda outputs: torch.tensor([torch.argmax(batch).item() for batch in outputs]).to(DEVICE),
             )
+
